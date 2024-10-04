fd0e94fe6b0cc5144e46900b0d6bd74dbf8f560b
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2024-10-04 23:12:48 +0200

    canvis realitzats

--- UtilString.java
+++
@@ -1,9 +0,0 @@
-public class UtilString {
-    // mètode que normalitza un String qualsevol i li treu els espais en blanc, convertint-los només en un espai en blanc
-    public static String normalitzaString(String cadena) {
-        if (cadena == null || cadena.strip().isEmpty()) {
-            throw new IllegalArgumentException("cadena no vàlid, sisplau ingresa un altre cadena.");
-        }
-        return cadena.replaceAll("\\s+", " ").strip();
-    }
-}
